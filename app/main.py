c24f47767f545dcd67540e20e8825b3a355bad33
Asman Malikov <asmanmalikov@gmail.com>
2022-11-09 14:56:34 +0400

    init project

--- app/main.py
+++ app/main.py
@@ -4,12 +4,12 @@ app = FastAPI()
 
 router = APIRouter()
 
+
 @router.get("/test/")
 def hello_world():
     return {
         'result': 'Hello World'
     }
 
+
 app.include_router(router, prefix="/api")
-
-
