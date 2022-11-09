43cfa77dfbbeca4799ac41a3db5f7c5c97085124
Asman Malikov <asmanmalikov@gmail.com>
2022-11-09 19:02:07 +0400

    init project

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, FastAPI
 from mangum import Mangum
 
-app = FastAPI(openapi_prefix='/dev')
+app = FastAPI(openapi_url='/dev/openapi.json')
 
 router = APIRouter(prefix='/dev')
 
