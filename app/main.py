6044cfd8aeded20dcbb9e4c5ee45145b785f3de5
Asman Malikov <asmanmalikov@gmail.com>
2022-11-11 00:13:06 +0400

    init api

--- app/main.py
+++ app/main.py
@@ -1,27 +1,16 @@
 """."""
+
 import os
 
-from fastapi import APIRouter, FastAPI
+from fastapi import FastAPI
 from mangum import Mangum
+
+from app.routes import router
 
 STAGE = os.environ.get('STAGE')
 
 root_path = '/' if not STAGE else f'/{STAGE}'
 app = FastAPI(root_path=root_path)
-
-router = APIRouter()
-
-
-@router.get('/hi')
-def get_hello_world():
-    return {'result': 'Hello World'}
-
-
-@router.post('/test')
-def post_hello_world(name: str):
-    return {
-        'result': f'Hello {name}',
-    }
 
 
 app.include_router(router, prefix='/api')
